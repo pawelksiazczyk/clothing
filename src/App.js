bbd705068b30b99be726e8b8d1806bdb48b37605
pawelksiazczyk <pawel_ksiazczyk@o2.pl>
2020-05-20 23:01:50 +0200

    Added smaller components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from "./components/home/Home"
+import Home from "./pages/homepage/Home"
 
 function App() {
   return (
---
+++ src/pages/homepage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./home.styles.scss";
+import Directory from "../../components/directory/Directory"
+
+const Home = () => {
+    return (
+      <div className="homepage">
+        <Directory/>
+      </div>
+    );
+}
+
+export default Home
