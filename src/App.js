364d827380c38fd5e8ffeec119e25932dd92e175
pawelksiazczyk <pawel_ksiazczyk@o2.pl>
2020-05-21 00:41:34 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from "./pages/homepage/Home";
-import Shop from "./pages/shop/Shop"
+import Shop from "./pages/shop/Shop";
 import { Route, Switch } from "react-router-dom";
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/shop" component={Shop} />
